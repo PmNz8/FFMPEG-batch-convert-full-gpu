de53bea6e9873a695d2afa71ed54f072c423c9d2
cichy45 <86913474+cichy45@users.noreply.github.com>
2023-05-15 10:10:49 +0200

    Update parameters

--- convert_qsv.ps1
+++ convert_qsv.ps1
@@ -21,16 +21,19 @@ while ($path -eq "" -or -not (Test-Path $path)) {
             $targetFull = "$($target)\$($parentfolder).hevc"   
         }
         #Write-Host "Creating new target directorty: $($targetFull)"
-        New-Item -ItemType Directory -Path "$targetFull"
+        if (Test-Path $targetFull) {
+        } else {
+            New-Item -ItemType Directory -Path "$targetFull"
+        }
 
-        $file_list = Get-ChildItem -Path $path -Include *.mkv -File -Recurse
+        $file_list = Get-ChildItem -Path $path -Include *.mkv, *.mp4 -File -Recurse -Exclude *hevc*
 
         foreach($file in $file_list)
         {
             #Write-Host "$($file.FullName)"
             $addon = "$($file.BaseName).hevc$($file.Extension)"
             #Write-Host "$($targetFull)\$($addon)"
-            & $ffmpeg\ffmpeg.exe -y -init_hw_device d3d11va=dx11:,vendor=0x8086 -init_hw_device qsv=qs@dx11 -filter_hw_device qs -hwaccel d3d11va -hwaccel_output_format d3d11 -i file:$($file.FullName) -map 0 -c:a copy -c:s copy -map -0:v:1 -threads 0 -codec:v:0 hevc_qsv -low_power 0 -preset veryfast -global_quality 23 -look_ahead 1 -look_ahead_depth 80 -vf "hwmap=derive_device=qsv,scale_qsv=format=p010" -profile:v main10 "$($targetFull)\$($addon)"
+            & $ffmpeg\ffmpeg.exe -y -init_hw_device d3d11va=dx11:,vendor=0x8086 -init_hw_device qsv=qs@dx11 -filter_hw_device qs -hwaccel d3d11va -hwaccel_output_format d3d11 -i file:$($file.FullName) -map 0 -c copy -map -0:v:1 -threads 0 -codec:v:0 hevc_qsv -low_power 0 -preset medium -global_quality 21 -look_ahead 1 -look_ahead_depth 80 -vf "hwmap=derive_device=qsv,scale_qsv=format=p010" -profile:v main10 "$($targetFull)\$($addon)"
 
         }
 
